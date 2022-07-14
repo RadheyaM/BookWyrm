c8cad24ac4d610575517ca1b912597f7f432398c
RadheyaM <radheyamansel@hotmail.com>
2022-07-14 11:44:27 +0000

    Fix html search bar, improve the fetch function in js and add ability to search google books by user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
-const bookCardTemplate = document.querySelector("[book-card-template]");
+// const bookCardTemplate = document.querySelector("[book-card-template]");
 
-fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter")
-  .then(res => res.json())
-  .then(data => console.log(data))
-
+// console.log(fetchData);
 
   // const card = bookCardTemplate.content.cloneNode(true).children[0]
   // console.log(card);
+
+  const userInput = document.getElementById("search");
+  const button = document.getElementById("search-button");
+
+  //query the google books api and return result
+  async function getBooks (searchInput) {
+    // const search = "harry+potter"
+
+    if (!searchInput) {
+      alert('Please enter a book title') 
+      return;
+    }
+
+    const endpoint = new URL(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
+    console.log(endpoint);
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    return data
+
+  }
+
+  button.addEventListener("click", () => {
+    getBooks(userInput.value);
+  })
