37f0732fc18504d0bb7883cfd47a8e2d501d66a1
RadheyaM <radheyamansel@hotmail.com>
2022-07-14 15:23:39 +0000

    HTML: tweak the template to fix errors preventing JS DOM interaction
    CSS: adjust some class names to correspond with new HTML
    
    JS: fix and simplify the code to generate populated book cards below the search bar upon hitting search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-// const bookCardTemplate = document.querySelector("[book-card-template]");
-
-// console.log(fetchData);
-
-  // const card = bookCardTemplate.content.cloneNode(true).children[0]
-  // console.log(card);
-
   const userInput = document.getElementById("search");
   const button = document.getElementById("search-button");
-  let responseData = {};
+  const bookCardTemplate = document.querySelector("#book-card-template");
+  const bookCardContainer = document.querySelector("#book-cards-container");  
+  //let responseData = {};
 
   //query the google books api and return results based on user input
   async function getBooks (searchInput) {
@@ -18,14 +13,28 @@
     }
 
     const endpoint = new URL(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
-    console.log(endpoint);
+    console.log(`You generated the following URL: ${endpoint}`);
+
     const response = await fetch(endpoint);
     const data = await response.json();
+    console.log("Your fetch request returned: ----------- ")
     console.log(data);
-    responseData = data;
+
+    //generate populated cards to display below search bar.
+    for (let i = 0; i < data.items.length; i++) {
+      console.log(data.items[i].volumeInfo.title)
+      const card = bookCardTemplate.content.cloneNode(true).children[0];
+      const bookDesc = card.querySelector("[book-desc]");
+      const bookTitle = card.querySelector("[book-title]");
+      bookDesc.textContent = data.items[i].volumeInfo.title;
+      bookTitle.textContent = data.items[i].volumeInfo.description;
+      bookCardContainer.append(card);
+    }
+
 
   }
 
-  button.addEventListener("click", () => {
-    getBooks(userInput.value);
-  })
+button.addEventListener("click", () => {
+  const responseData = getBooks("harry potter");
+  //renderDataToCards(responseData);
+})
