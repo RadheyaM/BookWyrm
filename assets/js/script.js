cedd27eeb6dfe7b37a8d559d7b8b2978fe82c268
RadheyaM <radheyamansel@hotmail.com>
2022-07-15 20:04:44 +0000

    Reduce the async function code size to call a series of smaller separate functions, to ultimately increase overall functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
-const responseData = getBooks(endpoint);
-const userInput = document.getElementById("search");
+let userInput = document.querySelector("#search");
+let userInputValue = "";
 const searchButton = document.getElementById("search-btn");
-let searchFieldToggle = document.getElementById("toggle-btn");
 const bookCardTemplate = document.querySelector("#book-card-template");
 const bookCardContainer = document.querySelector("#book-cards-container");  
 
+let searchFieldToggle = document.getElementById("toggle-btn");
+
+userInput.addEventListener("input", e => {
+  const value = e.target.value;
+  userInputValue = value;
+})
 
 //query the google books api and return results based on user input
-async function getBooks (searchInput) {
+async function performApiQuery(searchValue) {
 
-  if (searchInput === '') {
+  if (searchValue === '') {
     alert('Please enter a valid search term') 
     return;
   }
 
-  const endpoint = new URL(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&maxResults=40&projection=lite`);
+  const endpoint = new URL(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}&maxResults=40&projection=lite`);
   console.log(`You generated the following URL: ${endpoint}`);
 
   const response = await fetch(endpoint);
   const data = await response.json();
-  console.log("Your fetch request returned:")
   console.log(data);
-  return data;
+  const list = generateImageList(data);
+  //console.log(list);
+  generateHTMLCards(data, list);
 }
 
 //generate a list of image source pathways
@@ -36,20 +42,20 @@ function generateImageList(data) {
       imageUrls.push("assets/images/bookcover-placeholder.jpg");
     }
   }
-  console.log(imageUrls);
+  console.log(`image urls: ${imageUrls}`);
   return imageUrls;
 }
 
 
   //generate populated cards to display below search bar.
-function generateHTMLCards(data) {
+function generateHTMLCards(data, list) {
   for (let i = 0; i < data.items.length; i++) {
     const dataVolumeInfo = data.items[i].volumeInfo;
     const card = bookCardTemplate.content.cloneNode(true).children[0];
     const bookCover = card.querySelector("[book-cover]");
     const bookTitle = card.querySelector("[book-title]");
     const bookAuthor = card.querySelector("[book-auth]");
-    bookCover.style.background = `url(${imageUrls[i]}) no-repeat center`;
+    bookCover.style.background = `url(${list[i]}) no-repeat center`;
     bookTitle.textContent = dataVolumeInfo.title;
     bookAuthor.textContent = dataVolumeInfo.authors;
     bookCardContainer.append(card);
@@ -71,9 +77,6 @@ searchFieldToggle.addEventListener("click", () => {
   }
 })
 
-
-
 searchButton.addEventListener("click", () => {
-  getBooks(userInput.value);
-  //renderDataToCards(responseData);
-})
+  performApiQuery(userInputValue);
+});
